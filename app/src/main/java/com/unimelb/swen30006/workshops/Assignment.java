28c9dce65738cde01c0101abbd4377b298d11f0a
Liam Kell <liamtavita@gmail.com>
2025-03-28 10:13:42 +1100

    Added assignment class

--- app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
@@ -1,4 +1,21 @@
 package com.unimelb.swen30006.workshops;
+import java.util.Date;
+import java.util.List;
 
 public class Assignment {
+    private String description;
+    private Date dueDate;
+    private int maxAttempts;
+    private String name;
+
+    // Keep track of submissions
+    private List<Submission> validSubmissions = new ArrayList<>();
+    private List<Submission> invalidSubmissions = new ArrayList<>();
+
+    public Assignment(String description, Date dueDate, int maxAttempts, String name) {
+        this.description = description;
+        this.dueDate = dueDate;
+        this.maxAttempts = maxAttempts;
+        this.name = name;
+    }
 }
